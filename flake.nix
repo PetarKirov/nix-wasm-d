acdd64870fadf990201e3a5312144ba2580d9813
Petar Kirov <petar.p.kirov@gmail.com>
2026-02-17 18:22:39 +0200

    build(nix): Add `flake-parts`-based devshell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+    nix-src = {
+      url = "https://flakehub.com/f/DeterminateSystems/nix-src/3.16.0";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem =
+        { pkgs, inputs', ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.ldc
+              pkgs.dub
+              pkgs.lld
+              pkgs.wabt
+              inputs'.nix-src.packages.nix-cli
+            ];
+          };
+        };
+    };
+}
